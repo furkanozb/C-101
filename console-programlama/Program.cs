92751bdbb08138d8be2cd9a66b2e5134fd85edff
furkanozb <furkanozb37@gmail.com>
2021-06-21 17:39:19 +0300

    Main Methodu - Namespace kavramı - System Using Direktifleri - Run

---
+++ console-programlama/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace console_programlama
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            System.Console.WriteLine("İsiminizi giriniz: ");
+            string name= Console.ReadLine();
+            System.Console.WriteLine("Soyadınızı giriniz: ");
+            string surname= Console.ReadLine();
+            System.Console.WriteLine(  "Merhaba "+name+" "+surname);
+        }
+    }
+}
